806f74a9fdca760612ce4ab91eafb12dc77e5a16
Kelvin Lim <kelvin_lim@ctl.creative.com>
2018-03-20 16:44:56 +0800

    first commit

---
+++ app/src/main/java/com/example/myfile/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myfile;
+
+import android.app.Activity;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    private final String TAG = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
